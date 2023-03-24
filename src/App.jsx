83ff0592997704114416ec1971d059f56146a63a
uy-scuti100 <osenidirect@gmail.com>
2023-03-24 15:20:38 -0700

    p

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import FifthSection from "./Components/FifthSection";
-import FourthSection from "./Components/FourthSection";
 import Hero from "./Components/Hero";
 import MainSection from "./Components/MainSection";
 import MobileNav from "./Components/MobileNav";
@@ -9,15 +7,13 @@ import ThirdSection from "./Components/ThirdSection";
 
 function App() {
   return (
-    <div className="w-full bg-[#4C4C4C] mx-auto md:bg-[#212121] overflow-x-hidden relative">
+    <div className="w-full bg-#4C4C4C] mx-auto md:bg-[#212121] h-screen overflow-x-hidden relative">
       <Navbar />
       <MobileNav />
       <Hero />
       <MainSection />
       <SecondSection />
       <ThirdSection />
-      <FourthSection />
-      <FifthSection />
     </div>
   );
 }
